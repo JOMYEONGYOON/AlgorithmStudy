37d8848f2bb3467d1f91cc55e8178517b6579e6a
name <hairhold@gmail.com>
2021-11-09 20:45:34 +0900

    코테준비 알고리즘

---
+++ src/codingTest/Algorithm1.java
@@ -0,0 +1,24 @@
+package codingTest;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Algorithm1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char c = scanner.next().charAt(0);
+        int result = 0;
+        str = str.toUpperCase(Locale.ROOT);
+        str = str.toLowerCase(Locale.ROOT);
+        c = Character.toUpperCase(c);
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == c) {
+                result++;
+            }
+        }
+        System.out.println(result);
+
+
+    }
+}
