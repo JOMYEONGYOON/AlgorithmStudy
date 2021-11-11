7b69973d6dce681b90c5ccb2f246ad63d77cd023
name <hairhold@gmail.com>
2021-11-11 19:03:01 +0900

    코테준비 알고리즘

---
+++ src/codingTest/Algorithm4.java
@@ -0,0 +1,31 @@
+package codingTest;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Algorithm4 {
+    public ArrayList<String> solution(int n, String str[]) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            result.add(tmp);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Algorithm4 algorithm4 = new Algorithm4();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String str[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = scanner.next();
+        }
+        for(String result : algorithm4.solution(n, str)){
+            System.out.println(result);
+        }
+
+    }
+
+
+}
