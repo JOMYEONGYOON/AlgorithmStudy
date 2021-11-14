b7700668b44a785c6df543ba8eb32a46bd7d5e47
name <hairhold@gmail.com>
2021-11-14 19:48:42 +0900

    코테준비 알고리즘

--- src/codingTest/Algorithm7.java
+++ src/codingTest/Algorithm7.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 class Algorithm7 {
     public String solution(String str) {
         String result = "";
-        for (int i=0;i<str.length();i++){
-            if(i == str.indexOf(str.charAt(i))) {
-                result += str.charAt(i);
+
+        str = str.toUpperCase();
+
+        for (int i=0;i<str.length()/2;i++){
+            if(str.charAt(i) == str.charAt(str.length()-i-1)) {
+                result = "YES";
+            }else {
+                result = "NO";
             }
         }
         return result;
