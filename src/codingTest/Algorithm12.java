e9772f99b6b757eda146ff235264e4784622c24d
JOMYEONGYOON <hairhold@naver.com>
2021-11-19 20:16:34 +0900

    코딩 테스트 11월 19일

--- src/codingTest/Algorithm12.java
+++ src/codingTest/Algorithm12.java
@@ -3,29 +3,24 @@ package codingTest;
 import java.util.Scanner;
 
 class Algorithm12 {
-    public String solution(String str) {
+    public String solution(int cnt, String str2) {
         String result = "";
-        str = str+"";
-        int cnt = 1;
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) == str.charAt(i + 1)) {
-                cnt++;
-            } else {
-                result += str.charAt(i);
-                if (cnt > 1) {
-                    result += String.valueOf(cnt);
-                    cnt=1;
-                }
-            }
+        for(int i=0;i<str2.length();i++){
+            String tmp = str2.substring(0,7).replace('#','1').replace('*','0');
+            int num = Integer.parseInt(tmp,2);
+            result += (char)num;
+            str2 = str2.substring(7);
         }
+
         return result;
     }
 
     public static void main(String[] args) {
         Algorithm12 algorithm = new Algorithm12();
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
-        System.out.println(algorithm.solution(str));
+        int cnt = scanner.nextInt();
+        String str2 = scanner.next();
+        System.out.println(algorithm.solution(cnt,str2));
 
     }
 }
