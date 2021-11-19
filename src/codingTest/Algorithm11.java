0a45ce51b4ebb3412c8abda5ed9dff9df1655357
JOMYEONGYOON <hairhold@naver.com>
2021-11-19 12:18:33 +0900

    코딩 테스트 11월 19일

---
+++ src/codingTest/Algorithm11.java
@@ -0,0 +1,31 @@
+package codingTest;
+
+import java.util.Scanner;
+
+class Algorithm11 {
+    public String solution(String str) {
+        String result = "";
+        str = str+"";
+        int cnt = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            } else {
+                result += str.charAt(i);
+                if (cnt > 1) {
+                    result += String.valueOf(cnt);
+                    cnt=1;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Algorithm11 algorithm = new Algorithm11();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(algorithm.solution(str));
+
+    }
+}
