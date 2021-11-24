e7de673f44a7aef5397f68c31954b229183ee923
JOMYEONGYOON <hairhold@naver.com>
2021-11-24 17:08:40 +0900

    코딩 테스트 11월 24일

--- src/codingTest/Algorithm16.java
+++ src/codingTest/Algorithm16.java
@@ -1,37 +1,31 @@
 package codingTest;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Algorithm16 {
-    public String solution(int[] a, int[] b, int n) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if ((a[i] == 1 && b[i] == 3) || (a[i] == 2 && b[i] == 1) || (a[i] == 3 && b[i] == 2)) {
-                result.append("A");
-            } else if (a[i] == b[i]) {
-                result.append("D");
-            }else {
-                result.append("B");
-            }
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int a = 1;
+        int b = 1;
+        arrayList.add(a);
+        arrayList.add(b);
+        for(int i=2;i<n;i++){
+            int result = a+b;
+            arrayList.add(result);
+            a = b;
+            b = result;
         }
-        return result.toString();
+
+        return arrayList;
     }
 
     public static void main(String[] args) {
         Algorithm16 algorithm = new Algorithm16();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            b[i] = scanner.nextInt();
-        }
-        for (char result: algorithm.solution(a, b, n).toCharArray()
-             ) {
-            System.out.println(result);
+        for (int result:algorithm.solution(n)) {
+            System.out.print(result + " ");
         }
 
     }
