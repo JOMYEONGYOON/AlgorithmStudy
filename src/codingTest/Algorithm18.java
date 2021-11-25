3cfe5f75e245d3021b014d24e718da2fc5d3141d
JOMYEONGYOON <hairhold@naver.com>
2021-11-25 14:09:38 +0900

    코딩 테스트 11월 24일

---
+++ src/codingTest/Algorithm18.java
@@ -0,0 +1,29 @@
+package codingTest;
+
+import java.util.Scanner;
+
+class Algorithm18 {
+    public int solution(int n) {
+        int[] result = new int[n + 1];
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if(result[i] == 0) {
+                count++;
+                for(int j=i;j <= n ; j=j+i){
+                    result[j] = 1;
+                }
+
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Algorithm18 algorithm = new Algorithm18();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(algorithm.solution(n));
+
+    }
+}
