f10cbb2cf30fdf5aa4776c64b5a826b36f1f29d1
JOMYEONGYOON <hairhold@naver.com>
2021-11-27 10:21:08 +0900

    2021/11/27 코테준비

--- src/codingTest/Algorithm18.java
+++ src/codingTest/Algorithm18.java
@@ -1,19 +1,41 @@
 package codingTest;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Algorithm18 {
-    public int solution(int n) {
+    public boolean isPrime(int num) {
+        if(num ==1) return false;
+        for(int i=2; i<num; i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+    public ArrayList<Integer> solution(int n, int[] a) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int tmp = a[i];
+            int res = 0;
+            while(tmp>0){
+                int t = tmp%10;
+                res = res*10+t;
+                tmp /= 10;
+            }
+        }
 
-
-    return 0;
+        return result;
     }
 
     public static void main(String[] args) {
         Algorithm18 algorithm = new Algorithm18();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(algorithm.solution(n));
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = scanner.nextInt();
+        }
+        algorithm.solution(n,a);
+        System.out.println();
 
     }
 }
