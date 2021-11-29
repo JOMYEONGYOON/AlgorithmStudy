f70ceea81935ff5a41bd78de71cce9eb2a1b6235
JOMYEONGYOON <hairhold@naver.com>
2021-11-30 06:13:09 +0900

    2021/11/30 코테준비

---
+++ src/codingTest/Algorithm19.java
@@ -0,0 +1,27 @@
+package codingTest;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Algorithm19 {
+
+    public int solution(int n, int[] a) {
+        int result = 0;
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Algorithm19 algorithm = new Algorithm19();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        algorithm.solution(n, a);
+        System.out.println();
+
+    }
+}
