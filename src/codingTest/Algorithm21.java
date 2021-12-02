171e69c2e2fca82d3bce233e9258fa651b5d20e7
JOMYEONGYOON <hairhold@naver.com>
2021-12-02 14:46:35 +0900

    2021/12/02 코테준비

---
+++ src/codingTest/Algorithm21.java
@@ -0,0 +1,50 @@
+package codingTest;
+
+import java.util.Scanner;
+
+class Algorithm21 {
+
+    public int solution(int n, int[][] a) {
+        int result = Integer.MIN_VALUE;
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += a[i][j];
+                sum2 += a[j][i];
+            }
+            result = Math.max(result, sum1);
+            result = Math.max(result, sum2);
+        }
+
+        sum1 = 0;
+        sum2 = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum1 += a[i][i];
+            sum2 += a[i][n - i - 1];
+        }
+        result = Math.max(result, sum1);
+        result = Math.max(result, sum2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Algorithm21 algorithm = new Algorithm21();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(algorithm.solution(n, a));
+
+
+    }
+}
