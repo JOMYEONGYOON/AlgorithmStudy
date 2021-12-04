55099bb8ce23962d2c65d9df753f36a2ced4ded3
JOMYEONGYOON <hairhold@naver.com>
2021-12-04 17:03:57 +0900

    2021/12/04 코테준비

--- src/codingTest/Algorithm22.java
+++ src/codingTest/Algorithm22.java
@@ -3,25 +3,24 @@ package codingTest;
 import java.util.Scanner;
 
 class Algorithm22 {
+    int[] x = {-1, 0, 1, 0};
+    int[] y = {0, 1, 0, -1};
 
     public int solution(int n, int[][] a) {
-        int result = 0,max=0;
+        int result = 0;
 
-        for (int i = 1; i <= n; i++) {
-                int cnt = 0;
-            for (int j = 1; j <= n; j++) {
-                   for(int k=1;k<=5;k++){
-                       if(a[i][k] == a[j][k]){
-                           cnt++;
-                           break;
-                       }
-                   }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int x1 = i + x[k]; //
+                    int y2 = j + y[k]; //
+                    if( x1>=0 && x1<n && y2 >=0 && y2 < n && a[x1][y2] >= a[i][j]) {
+                        flag = false;
+                        break;
+                    }
                 }
-            if(cnt>max) {
-                max=cnt;
-                result=i;
-            }
-
+                if(flag) result++;
             }
         }
 
@@ -32,9 +31,9 @@ class Algorithm22 {
         Algorithm22 algorithm = new Algorithm22();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] a = new int[n+1][6];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = scanner.nextInt();
             }
         }
