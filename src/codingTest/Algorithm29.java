50f17084efbb143a1f0735874401daa19bf4036a
JOMYEONGYOON <hairhold@naver.com>
2021-12-12 21:41:39 +0900

    2021/12/12 코테준비

--- src/codingTest/Algorithm29.java
+++ src/codingTest/Algorithm29.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 
 class Algorithm29 {
 
-    public int solution(int n, int m, int[] arr) {
+    public int solution(int n,int[] arr) {
         int result = 0;
+        int sum = 0;
+        int lt = 0;
+        for(int i=0;i<n/2+1;i++){
+            sum += arr[i];
+            if(sum == n)  result++;
+            while(sum >= n) {
+                sum -= arr[lt++];
+                if(sum== n) result++;
+            }
+        }
         return result;
 }
 
@@ -14,12 +24,12 @@ class Algorithm29 {
         Algorithm29 algorithm = new Algorithm29();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = scanner.nextInt();
+        int m = n/2+1;
+        int[] arr = new int[m];
+        for(int i=0;i<m;i++){
+            arr[i] = i+1;
         }
-        System.out.println(algorithm.solution(n,m,arr));
+        System.out.println(algorithm.solution(n,arr));
 
     }
 }
