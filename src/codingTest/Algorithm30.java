026b9e2749788305bbd3c34727e66a46098d789c
JOMYEONGYOON <hairhold@naver.com>
2021-12-14 15:26:52 +0900

    2021/12/14 코테준비

--- src/codingTest/Algorithm30.java
+++ src/codingTest/Algorithm30.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 
 class Algorithm30 {
 
-    public int solution(int n,int[] arr) {
-        int result = 0;
+    public int solution(int n) {
+        int result = 0, cnt = 1;
+        n--;
+        while(n>0) {
+            cnt++;
+            n=n-cnt;
+            if(n%cnt==0) result++;
 
+        }
         return result;
 }
 
@@ -15,12 +21,7 @@ class Algorithm30 {
         Algorithm30 algorithm = new Algorithm30();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int m = n/2+1;
-        int[] arr = new int[m];
-        for(int i=0;i<m;i++){
-            arr[i] = i+1;
-        }
-        System.out.println(algorithm.solution(n,arr));
+        System.out.println(algorithm.solution(n));
 
     }
 }
