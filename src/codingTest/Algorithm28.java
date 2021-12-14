c9fdafb00169b0a9d448dc2d13c21a1831391c61
JOMYEONGYOON <hairhold@naver.com>
2021-12-14 15:29:10 +0900

    2021/12/14 코테준비

--- src/codingTest/Algorithm28.java
+++ src/codingTest/Algorithm28.java
@@ -22,12 +22,12 @@ class Algorithm28 {
 
     public static void main(String[] args) {
         Algorithm28 algorithm = new Algorithm28();
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
         int[] arr = new int[n];
         for(int i=0;i<n;i++){
-            arr[i] = scanner.nextInt();
+            arr[i] = scan.nextInt();
         }
         System.out.println(algorithm.solution(n,m,arr));
 
