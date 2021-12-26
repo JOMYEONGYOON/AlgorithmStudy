f72deac9931444a7e2431af261070c02942f97ca
JOMYEONGYOON <hairhold@naver.com>
2021-12-26 18:49:27 +0900

    2021/12/26 코테준비

---
+++ src/codingTest/Algorithm42.java
@@ -0,0 +1,23 @@
+package codingTest;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+
+class Algorithm42 {
+
+    public int solution(String str) {
+        int result = 0;
+        Stack<Character> stack = new Stack<>();
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Algorithm42 algorithm = new Algorithm42();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(algorithm.solution(str));
+
+    }
+}
