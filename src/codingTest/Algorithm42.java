5dbaddce963a74b91a96c879b3027b4776d3f242
JOMYEONGYOON <hairhold@naver.com>
2021-12-27 15:53:42 +0900

    2021/12/27 코테준비

--- src/codingTest/Algorithm42.java
+++ src/codingTest/Algorithm42.java
@@ -1,14 +1,24 @@
 package codingTest;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
 
 class Algorithm42 {
 
-    public int solution(String str) {
+    public int solution(int n,int k) {
         int result = 0;
-        Stack<Character> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1;i<=n;i++) queue.offer(i);
+            while(!queue.isEmpty()) {
+                for(int j=1; j<k;j++){
+                    queue.offer(queue.poll());
+                }
+                queue.poll();
+                if(queue.size() == 1) result = queue.poll();
+            }
 
         return result;
     }
@@ -16,8 +26,9 @@ class Algorithm42 {
     public static void main(String[] args) {
         Algorithm42 algorithm = new Algorithm42();
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
-        System.out.println(algorithm.solution(str));
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(algorithm.solution(n,k));
 
     }
 }
