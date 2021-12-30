7b4ceddcce8673f95322eb661f205da8fde9ae94
JOMYEONGYOON <hairhold@naver.com>
2021-12-30 19:54:14 +0900

    2021/12/30 코테준비

---
+++ src/codingTest/Algorithm44.java
@@ -0,0 +1,53 @@
+package codingTest;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+class Person{
+    int id;
+    int priority;
+    public Person(int id, int priority){
+        this.id=id;
+        this.priority=priority;
+    }
+}
+
+class Algorithm44 {
+
+    public int solution(int n, int m, int[] arr){
+        int result=0;
+        Queue<Person> Q=new LinkedList<>();
+        for(int i=0; i<n; i++){
+            Q.offer(new Person(i, arr[i]));
+        }
+        while(!Q.isEmpty()){
+            Person tmp=Q.poll();
+            for(Person x : Q){
+                if(x.priority>tmp.priority){
+                    Q.offer(tmp);
+                    tmp=null;
+                    break;
+                }
+            }
+            if(tmp!=null){
+                result++;
+                if(tmp.id==m) return result;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Algorithm44 algorithm = new Algorithm44();
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=in.nextInt();
+        }
+        System.out.println(algorithm.solution(n, m, arr));
+    }
+}
