6306dcdcb14eac16ca3379acfb40f0c2273e1d47
JOMYEONGYOON <hairhold@naver.com>
2022-01-01 12:52:02 +0900

    2022/01/01 코테준비

--- src/codingTest/Algorithm47.java
+++ src/codingTest/Algorithm47.java
@@ -3,23 +3,22 @@ package codingTest;
 import java.util.Scanner;
 
 
-class Algorithm46 {
+class Algorithm47 {
 
     public int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i -1; j++) {
-                if(arr[j] > arr[j+1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tmp;
-                }
-            }
+        for (int i = 1; i < n ; i++) {
+           int tmp = arr[i] ,j;
+           for(j=i-1;j>=0; j--){
+               if(arr[j] > tmp) arr[j+1] = arr[j];
+               else break;
+           }
+           arr[j+1] = tmp;
         }
         return arr;
     }
 
     public static void main(String[] args) {
-        Algorithm46 algorithm = new Algorithm46();
+        Algorithm47 algorithm = new Algorithm47();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
