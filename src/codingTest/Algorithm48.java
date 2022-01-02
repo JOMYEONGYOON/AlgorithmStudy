3939a68837a06487a30d8ed50c455d5baae5527e
JOMYEONGYOON <hairhold@naver.com>
2022-01-02 17:39:25 +0900

    2022/01/02 코테준비

---
+++ src/codingTest/Algorithm48.java
@@ -0,0 +1,33 @@
+package codingTest;
+
+import java.util.Scanner;
+
+
+class Algorithm48 {
+
+    public int[] solution(int n, int[] arr) {
+        for (int i = 1; i < n ; i++) {
+           int tmp = arr[i] ,j;
+           for(j=i-1;j>=0; j--){
+               if(arr[j] > tmp) arr[j+1] = arr[j];
+               else break;
+           }
+           arr[j+1] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Algorithm48 algorithm = new Algorithm48();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : algorithm.solution(n, arr)
+        ) {
+            System.out.print(x + " ");
+        }
+    }
+}
