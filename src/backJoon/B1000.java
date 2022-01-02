6287c7bb8dfd814b746e94bcc72f24b82399d2a8
JOMYEONGYOON <hairhold@naver.com>
2022-01-03 02:37:08 +0900

    2022/01/03 코테준비

---
+++ src/backJoon/B1000.java
@@ -0,0 +1,13 @@
+package backJoon;
+
+import java.util.*;
+
+public class B1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
+
---
+++ src/backJoon/B10950.java
@@ -0,0 +1,15 @@
+package backJoon;
+
+import java.util.Scanner;
+
+public class B10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=0;i<n;i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
