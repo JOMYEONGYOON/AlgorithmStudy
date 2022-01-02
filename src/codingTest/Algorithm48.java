7945fddabecd20fe17bf08fb23cb1652602f54ed
JOMYEONGYOON <hairhold@naver.com>
2022-01-02 17:41:11 +0900

    2022/01/02 코테준비

--- src/codingTest/Algorithm48.java
+++ src/codingTest/Algorithm48.java
@@ -5,29 +5,29 @@ import java.util.Scanner;
 
 class Algorithm48 {
 
-    public int[] solution(int n, int[] arr) {
-        for (int i = 1; i < n ; i++) {
-           int tmp = arr[i] ,j;
-           for(j=i-1;j>=0; j--){
-               if(arr[j] > tmp) arr[j+1] = arr[j];
-               else break;
-           }
-           arr[j+1] = tmp;
+    public int[] solution(int size, int[] arr) {
+        int[] cache = new int[size];
+        for (int x : arr) {
+            int pos = -1;
+            for (int i = 0; i < size; i++) if (x == cache[i]) pos = i;
+            if (pos == -1) {
+                if (size - 1 >= 0) System.arraycopy(cache, 0, cache, 1, size - 1);
+            } else {
+                System.arraycopy(cache, 0, cache, 1, pos);
+            }
+            cache[0] = x;
         }
-        return arr;
+        return cache;
     }
 
     public static void main(String[] args) {
         Algorithm48 algorithm = new Algorithm48();
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        Scanner kb = new Scanner(System.in);
+        int s = kb.nextInt();
+        int n = kb.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-        for (int x : algorithm.solution(n, arr)
-        ) {
-            System.out.print(x + " ");
-        }
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        for (int x : algorithm.solution(s, arr)) System.out.print(x + " ");
     }
 }
+
