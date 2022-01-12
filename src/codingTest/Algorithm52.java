a524a1d9656bd8ed5059b1be675a542b3d8bc612
JOMYEONGYOON <hairhold@naver.com>
2022-01-12 17:29:06 +0900

    2022/01/12 코테준비

--- src/codingTest/Algorithm52.java
+++ src/codingTest/Algorithm52.java
@@ -1,20 +1,23 @@
 package codingTest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 
 class Algorithm52 {
 
-    public ArrayList<Integer> solution(int size, int[] arr) {
-        ArrayList<Integer> result = new ArrayList<>();
-        int tmp[] = arr.clone();
-        Arrays.sort(tmp);
-        for(int i=0;i<size;i++){
-            if(tmp[i] != arr[i]) {
-               result.add(i+1);
+    public int solution(int n, int m, int[] arr) {
+        int result = 0;
+        int lt = 0, rt = n - 1;
+        Arrays.sort(arr);
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+                if(arr[mid] == m){
+                    result = mid +1;
+                    break;
             }
+                if(arr[mid] > m) rt = mid-1;
+                else lt = mid+1;
         }
         return result;
     }
@@ -23,11 +26,10 @@ class Algorithm52 {
         Algorithm52 algorithm = new Algorithm52();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
+        int m = kb.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
-        for (int result: algorithm.solution(n,arr)) {
-            System.out.print(result + " ");
-        }
+        System.out.print(algorithm.solution(n, m, arr));
     }
 }
 
