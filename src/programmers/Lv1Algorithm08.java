159c5f7d1bf2af9cd40b75159a46a67adde65a1e
jomyungyoon <hairhold@naver.com>
2022-02-05 19:43:07 +0900

    2022/02/05

---
+++ src/programmers/Lv1Algorithm08.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Lv1Algorithm08 {
+
+    public int solution() {
+        int result = 0;
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Lv1Algorithm08 main = new Lv1Algorithm08();
+
+    }
+}
